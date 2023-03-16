11298cddd1506b708f74076718c4e2d40de9a122
Arpan-Ghosh9298 <arpan@techis.io>
2023-03-17 00:15:05 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './Components/header';
+import Buttons from './Components/buttons';
+import Content from './Components/Content';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+     < Header />
+     <Buttons/>
+     <Content/>
+    </div>
+  );
+}
+
+export default App;
